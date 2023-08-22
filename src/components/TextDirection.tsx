08dd5c93fdeaf44a9ea840ec019e4ce86c8dbe77
Spencer Miller <dogpantz@gmail.com>
2023-08-22 11:51:23 -0600

    refactor TextDirection component into ui-core

---
+++ src/components/TextDirection.tsx
@@ -0,0 +1,33 @@
+// ** React Imports
+import { useEffect } from 'react'
+
+// ** Emotion Imports
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+
+// ** RTL Plugin
+import stylisRTLPlugin from 'stylis-plugin-rtl'
+
+const styleCache = () =>
+  createCache({
+  key: 'rtl',
+  prepend: true,
+  stylisPlugins: [stylisRTLPlugin]
+})
+
+type Props = {
+  direction: string
+  children: React.ReactNode
+}
+
+export const TextDirection = (props: Props) => {
+  const { children, direction } = props
+  useEffect(() => {
+    document.dir = direction
+  }, [direction])
+  if (direction === 'rtl') {
+    return <CacheProvider value={styleCache()}>{children}</CacheProvider>
+  }
+
+  return <>{children}</>
+}
