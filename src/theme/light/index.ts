503e6538643d68cfbbb71cd1770d5f28c388670e
Spencer Miller <dogpantz@gmail.com>
2023-08-30 16:43:59 -0600

    DEVKIN-555 - add theming logic

---
+++ src/theme/light/index.ts
@@ -0,0 +1,73 @@
+import { colors } from './colors'
+import { ColorMap } from '../types'
+
+export const map: ColorMap = {
+  neutral: {
+    white: colors.neutral900,
+    lightest: colors.neutral800,
+    lighter: colors.neutral700,
+    light: colors.neutral600,
+    medium: colors.neutral500,
+    dark: colors.neutral400,
+    darker: colors.neutral300,
+    darkest: colors.neutral200,
+    black: colors.neutral100,
+  },
+  primary: {
+    lighter: colors.cyan500,
+    light: colors.cyan400,
+    medium: colors.cyan300,
+    dark: colors.cyan200,
+    darker: colors.cyan100,
+  },
+  secondary: {
+    lighter: colors.magenta500,
+    light: colors.magenta400,
+    medium: colors.magenta300,
+    dark: colors.magenta200,
+    darker: colors.magenta100,
+  },
+  tertiary: {
+    lighter: colors.purple500,
+    light: colors.purple400,
+    medium: colors.purple300,
+    dark: colors.purple200,
+    darker: colors.purple100,
+  },
+  contrast: {
+    lighter: colors.green500,
+    light: colors.green400,
+    medium: colors.green300,
+    dark: colors.green200,
+    darker: colors.green100,
+  },
+  success: {
+    lighter: colors.green500,
+    light: colors.green400,
+    medium: colors.green300,
+    dark: colors.green200,
+    darker: colors.green100,
+  },
+  warning: {
+    lighter: colors.yellow500,
+    light: colors.yellow400,
+    medium: colors.yellow300,
+    dark: colors.yellow200,
+    darker: colors.yellow100,
+  },
+  error: {
+    lighter: colors.red500,
+    light: colors.red400,
+    medium: colors.red300,
+    dark: colors.red200,
+    darker: colors.red100,
+  },
+  info: {
+    lighter: colors.cyan500,
+    light: colors.cyan400,
+    medium: colors.cyan300,
+    dark: colors.cyan200,
+    darker: colors.cyan100,
+  },
+}
+
