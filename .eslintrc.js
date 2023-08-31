803c10fdb6d5dc7d8de9df5c898ad22507eabce2
Spencer Miller <dogpantz@gmail.com>
2023-08-31 15:43:36 -0600

    improve rules and parsing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,34 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'eslint-config-airbnb'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-airbnb',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: './',
+  },
+
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      'typescript': {
+        'alwaysTryTypes': true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      }
+    }
+  },
+
+  plugins: ['import', '@typescript-eslint', 'react'],
   root: true,
 
   rules: {
-    'semi': ['error', 'never'],
-  }
+    semi: ['error', 'never'],
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+  },
 }
