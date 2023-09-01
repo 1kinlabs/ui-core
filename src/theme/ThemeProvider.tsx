ff9868111697f8ef43b8c5fba5f6c03493b19158
Spencer Miller <dogpantz@gmail.com>
2023-08-31 20:26:42 -0600

    fix committed merge conflict markers

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,6 +1,5 @@
 import { useMemo, useEffect } from 'react'
 import { ThemeProvider as EmotionThemeProvider } from '@emotion/react'
-import dynamic from 'next/dynamic'
 import { useStorage } from 'hooks/useStorage'
 import { supportedThemes, SupportedTheme, themeEvents } from './types'
 
@@ -8,26 +7,15 @@ type Props = {
   children: React.ReactNode
 }
 
-<<<<<<< HEAD
-export const ThemeProvider = ({ children }: Props) => {
-=======
 export function ThemeProvider({ children }: Props) {
->>>>>>> d496d8f (fix issues preventing ThemeProvider from working)
   const [currentTheme, setCurrentTheme] = useStorage('currentTheme')
 
   const theme = useMemo(() => {
     if (!supportedThemes[currentTheme as SupportedTheme]) {
       setCurrentTheme(supportedThemes.default)
-<<<<<<< HEAD
-      return dynamic(() => import(`theme/${supportedThemes.default}`))
-    } else {
-      return dynamic(() => import(`theme/${currentTheme}`))
-    }
-=======
       return () => import(`./${supportedThemes.default}`)
     }
     return () => import(`./${currentTheme}`)
->>>>>>> d496d8f (fix issues preventing ThemeProvider from working)
   }, [currentTheme])
 
   useEffect(() => {
