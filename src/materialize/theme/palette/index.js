706e8d50e727b3ecdde6eb4023d50888efb60d05
Spencer Miller <dogpantz@gmail.com>
2023-09-01 16:59:22 -0600

    map mui theme to new theme

--- src/materialize/theme/palette/index.js
+++ src/materialize/theme/palette/index.js
@@ -1,96 +1,88 @@
-const DefaultPalette = (mode, skin) => {
+import { map } from 'theme/default'
+
+const DefaultPalette = (mode) => {
   // ** Vars
   const lightColor = '76, 78, 100'
   const darkColor = '234, 234, 255'
   const mainColor = mode === 'light' ? lightColor : darkColor
-
-  const defaultBgColor = () => {
-    if (skin === 'bordered' && mode === 'light') {
-      return '#FFF'
-    } if (skin === 'bordered' && mode === 'dark') {
-      return '#30334E'
-    } if (mode === 'light') {
-      return '#F7F7F9'
-    } return '#282A42'
-  }
 
   return {
     customColors: {
       dark: darkColor,
       main: mainColor,
       light: lightColor,
-      darkBg: '#282A42',
-      lightBg: '#F7F7F9',
-      bodyBg: mode === 'light' ? '#F7F7F9' : '#282A42',
+      darkBg: map.neutral.darker,
+      lightBg: map.neutral.dark,
+      bodyBg: map.neutral.darker,
       tooltipBg: mode === 'light' ? '#262732' : '#464A65',
       tableHeaderBg: mode === 'light' ? '#F5F5F7' : '#3A3E5B',
     },
     common: {
-      black: '#000',
-      white: '#FFF',
+      black: map.neutral.black,
+      white: map.neutral.white,
     },
     mode,
     primary: {
-      light: '#787EFF',
-      main: '#666CFF',
-      dark: '#5A5FE0',
-      contrastText: '#FFF',
+      light: map.primary.medium,
+      main: map.primary.medium,
+      dark: map.primary.dark,
+      contrastText: map.neutral.black,
     },
     secondary: {
-      light: '#7F889B',
-      main: '#6D788D',
-      dark: '#606A7C',
-      contrastText: '#FFF',
+      light: map.secondary.light,
+      main: map.secondary.medium,
+      dark: map.secondary.dark,
+      contrastText: map.neutral.white,
     },
     success: {
-      light: '#83E542',
-      main: '#72E128',
-      dark: '#64C623',
-      contrastText: '#FFF',
+      light: map.success.light,
+      main: map.success.medium,
+      dark: map.success.dark,
+      contrastText: map.neutral.white,
     },
     error: {
-      light: '#FF625F',
-      main: '#FF4D49',
-      dark: '#E04440',
-      contrastText: '#FFF',
+      light: map.error.light,
+      main: map.error.medium,
+      dark: map.error.dark,
+      contrastText: map.neutral.white,
     },
     warning: {
-      light: '#FDBE42',
-      main: '#FDB528',
-      dark: '#DF9F23',
-      contrastText: '#FFF',
+      light: map.warning.light,
+      main: map.warning.medium,
+      dark: map.warning.dark,
+      contrastText: map.neutral.white,
     },
     info: {
-      light: '#40CDFA',
-      main: '#26C6F9',
-      dark: '#21AEDB',
-      contrastText: '#FFF',
+      light: map.info.light,
+      main: map.info.medium,
+      dark: map.info.dark,
+      contrastText: map.neutral.white,
     },
     grey: {
-      50: '#FAFAFA',
-      100: '#F5F5F5',
-      200: '#EEEEEE',
-      300: '#E0E0E0',
-      400: '#BDBDBD',
-      500: '#9E9E9E',
-      600: '#757575',
-      700: '#616161',
-      800: '#424242',
-      900: '#212121',
-      A100: '#D5D5D5',
-      A200: '#AAAAAA',
-      A400: '#616161',
-      A700: '#303030',
+      50: map.neutral.white,
+      100: map.neutral.lightest,
+      200: map.neutral.lighter,
+      300: map.neutral.lighter,
+      400: map.neutral.light,
+      500: map.neutral.light,
+      600: map.neutral.medium,
+      700: map.neutral.medium,
+      800: map.neutral.dark,
+      900: map.neutral.dark,
+      A100: map.neutral.darker,
+      A200: map.neutral.darker,
+      A400: map.neutral.darkest,
+      A700: map.neutral.darkest,
     },
     text: {
-      primary: `rgba(${mainColor}, 0.87)`,
-      secondary: `rgba(${mainColor}, 0.68)`,
-      disabled: `rgba(${mainColor}, 0.38)`,
+      primary: map.neutral.lighter,
+      secondary: map.neutral.dark,
+      disabled: map.neutral.medium,
     },
     divider: `rgba(${mainColor}, 0.12)`,
     background: {
-      paper: mode === 'light' ? '#FFF' : '#30334E',
-      default: defaultBgColor(),
+      paper: map.neutral.darker,
+      default: map.neutral.darkest,
     },
     action: {
       active: `rgba(${mainColor}, 0.54)`,
