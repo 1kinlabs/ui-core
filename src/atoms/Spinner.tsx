ac6bc3ebc239ac44458ec44bcda558bdcf11d0e7
Spencer Miller <dogpantz@gmail.com>
2023-10-05 02:08:48 -0600

    prevent activeTheme from ever being undefined, also add Spinner atom

---
+++ src/atoms/Spinner.tsx
@@ -0,0 +1,27 @@
+// ** MUI Import
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
+import { styled } from 'theme'
+import { Logo as BaseLogo } from 'components/Logo'
+
+const Logo = styled(BaseLogo)`
+  width: 80px;
+  height: 80px;
+`
+
+export function Spinner() {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <Logo small />
+      <CircularProgress disableShrink sx={{ mt: 6 }} />
+    </Box>
+  )
+}
