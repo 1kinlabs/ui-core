3f71869bd29c1ce143bea8276ca8f48681d45c7b
Spencer Miller <dogpantz@gmail.com>
2023-10-16 17:34:06 -0600

    add storybook (#32)
    * add storybook
    
    * remove autogenerated crap
    
    * remove accidentally committed examples...
    
    * add .storybook to eslintignore

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button } from 'atoms/Button'
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'atoms/Button',
+  component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    children: {
+      control: 'text',
+      description: 'The content of the button',
+    },
+    variant: {
+      control: 'text',
+      description: 'The variant of the button (from MUI)',
+    },
+  },
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    children: 'Click Me!',
+  },
+}
