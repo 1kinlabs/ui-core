14b091ec23b865d71e01da0960852b9a4161f0a0
Danny North <iamdnorth@gmail.com>
2023-10-24 19:06:17 -0600

    Add item grid component and update styles (#35)

--- src/mock-data/game.ts
+++ src/mock-data/game.ts
@@ -1,6 +1,73 @@
 import { Game } from 'types/Game'
 import { Platform, Genre, GameMode } from 'enums/Game'
 
+export const pineapplePizzaHero : Game = {
+  id: 110,
+  title: 'Pineapple Pizza Hero',
+  disabled: true,
+  description: "\n\"Pineapple Pizza Hero\" is an enchanting 3D platformer that transports players into a vibrant world bursting with color and whimsy!\n\nArmed with a trusty sword and an unwavering spirit, navigate through a series of intricate levels, battling quirky foes and uncovering hidden secrets. But it's not just about the adventure; it's a quest to prove that pineapples truly belong on pizza!\n\nDive into a universe where every jump, swing, and slice counts. Are you ready to champion the cause of pineapple pizzas everywhere? Dive in and let the juicy adventure begin! 🍍🍕✨",
+  min_age: 6,
+  platforms: [
+    Platform.PC,
+    Platform.IOS,
+    Platform.WEB_BROWSER,
+    Platform.MAC,
+    Platform.ANDROID,
+  ],
+  publisher: '1Kin Labs',
+  primary_developer: '',
+  other_developers: [],
+  genres: [
+    Genre.PLATFORMER,
+    Genre.CASUAL,
+    Genre.ACTION,
+  ],
+  game_modes: [
+    GameMode.SINGLE_PLAYER,
+    GameMode.MULTIPLAYER,
+  ],
+  release_date: '2022-07-07',
+  faq_list: [
+    {
+      answer: 'Absolutely! "Pineapple Pizza Hero" is designed with players of all ages in mind. Its family-friendly graphics, non-violent gameplay, and charming storyline make it perfect for young gamers, while its intricate levels and challenges appeal to older players as well.',
+      question: 'Is "Pineapple Pizza Hero" suitable for kids?',
+    },
+    {
+      answer: "\"Pineapple Pizza Hero\" is currently available on PC, Mac, Browser, and mobile devices! We're also excited to announce an upcoming port to gaming consoles. Stay tuned for updates on our official website.",
+      question: 'What platforms is "Pineapple Pizza Hero" available on?',
+    },
+  ],
+  vendor_icon: {
+    defaultMedia: {
+      src1x: 'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/1KIN-Icon-RGB_4004228_small.png',
+      src2x: 'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/1KIN-Icon-RGB_4004228_large.png',
+    },
+  },
+  cover_art: {
+    defaultMedia: {
+      src1x: 'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/pineapple_pizza_hero_box_art_compact_f31c70728b.jpg',
+      src2x: 'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/pineapple_pizza_hero_box_art_compact_f31c70728b.jpg',
+    },
+  },
+  additional_media: null,
+  account_link_config: {
+    linkingUrl: 'https://pineapplepizzahero.1kin.io',
+  },
+  official_website: 'https://pineapplepizzahero.1kin.io',
+  createdAt: '2023-10-24T06:58:12.120Z',
+  updatedAt: '2023-10-24T06:58:35.388Z',
+  deleted: false,
+  person_name: 'Danny North',
+  official_email: 'pineapplepizzaherogame@1kin.io',
+  game_uid: '2c006514b2acbbc096af',
+  onchain_game_id: null,
+  liveContent: 4,
+  claims: {
+    totalContent: 9,
+    liveContent: 2,
+  },
+}
+
 export const pineapplePizzaTrivia : Game = {
   id: 22,
   title: 'Pineapple Pizza Trivia',
---
+++ src/stories/ItemGrid.stories.tsx
@@ -0,0 +1,194 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ItemGrid from 'components/ItemGrid'
+import GameCard from 'components/cards/GameCard'
+import CollectibleCard from 'components/cards/CollectibleCard'
+import { ClaimStatus } from 'enums/ClaimStatus'
+import {
+  pineapplePizzaHeroCard,
+  shieldOfTheAncients,
+  shiningSword,
+  pipTheEnchantress,
+  sidekickHopple,
+  cloakOfTheAges,
+} from 'mock-data/collectible'
+import { pineapplePizzaHero, pineapplePizzaTrivia } from 'mock-data/game'
+
+const meta = {
+  title: 'components/ItemGrid',
+  component: ItemGrid,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ItemGrid>
+
+export default meta
+type Story = StoryObj<typeof meta>;
+
+const longListArgs = {
+  children: [
+    <CollectibleCard
+      collectible={pineapplePizzaHeroCard}
+      key={pineapplePizzaHeroCard.id}
+      claimStatus={ClaimStatus.COMPLETED}
+    />,
+    <CollectibleCard collectible={shieldOfTheAncients} key={shieldOfTheAncients.id} />,
+    <CollectibleCard
+      collectible={{ ...sidekickHopple, available_codes: 0, auto_generate_claim_code: false }}
+      key={sidekickHopple.id}
+      claimStatus={ClaimStatus.COMPLETED}
+    />,
+    <CollectibleCard
+      collectible={{ ...pineapplePizzaHeroCard, title: 'Pineapple Pizza Hero' }}
+      key={pineapplePizzaHeroCard.id}
+      claimStatus={ClaimStatus.IN_PROGRESS}
+    />,
+
+    <CollectibleCard
+      collectible={{
+        ...shiningSword, title: 'Sword of Living', status: 'Live', available_codes: 0, auto_generate_claim_code: false,
+      }}
+      key={shiningSword.id}
+    />,
+    <CollectibleCard
+      collectible={pipTheEnchantress}
+      key={pipTheEnchantress.id}
+      claimStatus={ClaimStatus.IN_PROGRESS}
+    />,
+    <CollectibleCard
+      collectible={cloakOfTheAges}
+      key={cloakOfTheAges.id}
+    />,
+    <CollectibleCard collectible={shieldOfTheAncients} key={shieldOfTheAncients.id} />,
+    <CollectibleCard
+      collectible={pipTheEnchantress}
+      key={pipTheEnchantress.id}
+      claimStatus={ClaimStatus.IN_PROGRESS}
+    />,
+    <CollectibleCard
+      collectible={sidekickHopple}
+      key={sidekickHopple.id}
+      claimStatus={ClaimStatus.COMPLETED}
+    />,
+    <CollectibleCard
+      collectible={{ ...pipTheEnchantress, title: 'Esmerelda the Wizard', status: 'Archived' }}
+      key={pipTheEnchantress.id}
+    />,
+    <CollectibleCard collectible={{ ...shieldOfTheAncients, title: 'Shield of the Ancients - Legacy', status: 'Archived' }} key={shieldOfTheAncients.id} />,
+    <CollectibleCard collectible={shiningSword} key={shiningSword.id} />,
+  ],
+}
+
+export const Default: Story = {
+  args: {
+    children: [
+      <CollectibleCard
+        collectible={pineapplePizzaHeroCard}
+        key={pineapplePizzaHeroCard.id}
+        claimStatus={ClaimStatus.COMPLETED}
+      />,
+      <CollectibleCard
+        collectible={cloakOfTheAges}
+        key={cloakOfTheAges.id}
+        claimStatus={ClaimStatus.COMPLETED}
+      />,
+      <CollectibleCard collectible={shieldOfTheAncients} key={shieldOfTheAncients.id} />,
+      <CollectibleCard
+        collectible={pipTheEnchantress}
+        key={pipTheEnchantress.id}
+        claimStatus={ClaimStatus.IN_PROGRESS}
+      />,
+      <CollectibleCard
+        collectible={{ ...sidekickHopple, available_codes: 0, auto_generate_claim_code: false }}
+        key={sidekickHopple.id}
+        claimStatus={ClaimStatus.COMPLETED}
+      />,
+      <CollectibleCard collectible={shiningSword} key={shiningSword.id} />,
+
+    ],
+  },
+}
+
+export const LongList: Story = {
+  args: longListArgs,
+}
+
+export const LongListTablet: Story = {
+  args: longListArgs,
+  parameters: {
+    viewport: {
+      defaultViewport: 'tablet',
+    },
+  },
+}
+
+export const MobileCollectibleCard: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile2',
+    },
+  },
+  args: {
+    children: [
+      <CollectibleCard
+        collectible={pineapplePizzaHeroCard}
+        key={pineapplePizzaHeroCard.id}
+        claimStatus={ClaimStatus.COMPLETED}
+      />,
+      <CollectibleCard
+        collectible={cloakOfTheAges}
+        key={cloakOfTheAges.id}
+        claimStatus={ClaimStatus.COMPLETED}
+      />,
+      <CollectibleCard collectible={shieldOfTheAncients} key={shieldOfTheAncients.id} />,
+      <CollectibleCard
+        collectible={pipTheEnchantress}
+        key={pipTheEnchantress.id}
+        claimStatus={ClaimStatus.IN_PROGRESS}
+      />,
+      <CollectibleCard
+        collectible={{ ...sidekickHopple, available_codes: 0, auto_generate_claim_code: false }}
+        key={sidekickHopple.id}
+        claimStatus={ClaimStatus.COMPLETED}
+      />,
+      <CollectibleCard collectible={shiningSword} key={shiningSword.id} />,
+    ],
+  },
+}
+
+export const MobileGameCard: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile2',
+    },
+  },
+  args: {
+    children: [
+      <GameCard
+        game={pineapplePizzaHero}
+        key={pineapplePizzaHero.id}
+      />,
+      <GameCard
+        game={pineapplePizzaTrivia}
+        key={pineapplePizzaTrivia.id}
+      />,
+      <GameCard
+        game={pineapplePizzaHero}
+        key={pineapplePizzaHero.id}
+      />,
+      <GameCard
+        game={pineapplePizzaTrivia}
+        key={pineapplePizzaTrivia.id}
+      />,
+      <GameCard
+        game={pineapplePizzaHero}
+        key={pineapplePizzaHero.id}
+      />,
+      <GameCard
+        game={pineapplePizzaTrivia}
+        key={pineapplePizzaTrivia.id}
+      />,
+    ],
+  },
+}
