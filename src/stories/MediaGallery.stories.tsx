ecb0801ded458c43ba6bfa84f3b62f8eefaacbbf
Danny North <iamdnorth@gmail.com>
2023-11-03 18:45:55 -0600

    Make long list story

--- src/stories/MediaGallery.stories.tsx
+++ src/stories/MediaGallery.stories.tsx
@@ -19,3 +19,28 @@ export const Default: Story = {
     ],
   },
 }
+
+export const LargeGallery: Story = {
+  args: {
+    images: [
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/1_c718c8b66c.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/6_b621031e7f.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/8_5ab01e5af9.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/1_c718c8b66c.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/6_b621031e7f.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/8_5ab01e5af9.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/1_c718c8b66c.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/6_b621031e7f.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/8_5ab01e5af9.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/1_c718c8b66c.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/6_b621031e7f.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/8_5ab01e5af9.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/1_c718c8b66c.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/6_b621031e7f.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/8_5ab01e5af9.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/1_c718c8b66c.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/6_b621031e7f.png',
+      'https://1kin-dashboard-bucket.s3.us-east-2.amazonaws.com/8_5ab01e5af9.png',
+    ],
+  },
+}
