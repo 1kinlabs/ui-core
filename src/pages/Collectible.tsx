9962a7649ae8f2dc90c36be080c3aaea9fdb48b9
Danny North <iamdnorth@gmail.com>
2023-11-08 23:53:24 -0700

    Add basic media queries (these will need to be changed to container queries)

--- src/pages/Collectible.tsx
+++ src/pages/Collectible.tsx
@@ -4,6 +4,7 @@ import FAQ from 'components/FAQ'
 import GameInfo from 'components/game/GameInfo'
 import GameLauncher from 'components/game/GameLauncher'
 import MediaGallery from 'components/media/MediaGallery'
+import { mobileSmall, tablet } from 'css/media'
 import styled from 'styled-components'
 import { Collectible as CollectibleTypes } from 'types/Collectible'
 import { Game } from 'types/Game'
@@ -14,28 +15,34 @@ export type Props = {
   game: Game
 }
 
-const Collectible = styled(({ className, collectible, game } : Props) => (
-  <div className={className}>
-    <Section title="Description" className="description">
-      <Typography variant="body2">
-        {collectible.description}
-      </Typography>
-    </Section>
-    <Section title="What's Included" className="included">
-      <Typography variant="body2">
-        {collectible.item_details}
-      </Typography>
-    </Section>
-    <GameLauncher game={game} className="launcher" />
-    <Section className="gallery">
-      {
+const Collectible = styled(({ className, collectible, game } : Props) => {
+  const faqList = [...(collectible.faq_list || []), ...(game.faq_list || [])]
+
+  return (
+    <div className={className}>
+      <Section title="Description" className="description">
+        <Typography variant="body2">
+          {collectible.description}
+        </Typography>
+      </Section>
+      <Section title="What's Included" className="included">
+        <Typography variant="body2">
+          {collectible.item_details}
+        </Typography>
+      </Section>
+      <GameLauncher game={game} className="launcher" />
+      <Section className="gallery">
+        {
         collectible.assets?.additionalMedia && <MediaGallery images={collectible.assets.additionalMedia} />
       }
-    </Section>
-    <GameInfo game={game} className="info" />
-    <FAQ faqs={[...collectible.faq_list, ...game.faq_list]} className="faq" />
-  </div>
-))`
+      </Section>
+      <GameInfo game={game} className="info" />
+      {
+        faqList.length > 0 && <FAQ faqs={faqList} className="faq" />
+      }
+    </div>
+  )
+})`
   display: grid;
   gap: 16px;
   max-width: 1200px;
@@ -46,6 +53,27 @@ const Collectible = styled(({ className, collectible, game } : Props) => (
   "gallery gallery gallery"
   "info info info"
   "faq faq faq";
+
+  ${tablet} {
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas:
+    "description description"
+    "included launcher"
+    "gallery gallery"
+    "info info"
+    "faq faq";
+  }
+
+  ${mobileSmall} {
+    grid-template-columns: 1fr;
+    grid-template-areas:
+    "description"
+    "included"
+    "launcher"
+    "gallery"
+    "info"
+    "faq";
+  }
 
   .description {
     grid-area: description;
