b7adfc3a2f774ba9fab0fa312da41c19b4d6fc31
Danny North <iamdnorth@gmail.com>
2023-11-09 12:16:27 -0700

    Update chip to allow outline

--- src/atoms/Chip.tsx
+++ src/atoms/Chip.tsx
@@ -8,16 +8,22 @@ type EntryType = {
 }
 
 export enum TypeKey {
+  AVAILABLE,
   IN_PROGRESS,
-  SOLD_OUT
+  COMPLETED,
+  SOLD_OUT,
+  EXPIRED
 }
 
 const useTypeInfo = (type : TypeKey) : EntryType => {
   const theme = useTheme()
 
   const typeMap = {
+    [TypeKey.AVAILABLE]: { backgroundColor: theme.text.link, color: theme.surface.body, text: 'Available' },
     [TypeKey.IN_PROGRESS]: { backgroundColor: theme.text.warning, color: theme.surface.body, text: 'In Progress' },
+    [TypeKey.COMPLETED]: { backgroundColor: theme.text.success, color: theme.surface.body, text: 'Completed' },
     [TypeKey.SOLD_OUT]: { backgroundColor: theme.text.error, color: theme.text.primary, text: 'Sold Out' },
+    [TypeKey.EXPIRED]: { backgroundColor: theme.text.disabled, color: theme.surface.body, text: 'Expired' },
   }
 
   return typeMap[type]
@@ -25,17 +31,19 @@ const useTypeInfo = (type : TypeKey) : EntryType => {
 
 export type Props = {
   type: TypeKey,
-  className?: string
+  className?: string,
+  outline?: boolean
 }
 
-const Chip = styled(({ type, className } : Props) => {
+const Chip = styled(({ type, className, outline } : Props) => {
   const { backgroundColor, color, text } = useTypeInfo(type)
 
   return (
     <Box
       sx={{
-        backgroundColor,
-        color,
+        backgroundColor: outline ? 'transparent' : backgroundColor,
+        color: outline ? backgroundColor : color,
+        border: outline ? `1px solid ${backgroundColor}` : '',
       }}
       className={className}
     >
--- src/stories/atoms/Chip.stories.tsx
+++ src/stories/atoms/Chip.stories.tsx
@@ -14,14 +14,68 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>;
 
+export const Available: Story = {
+  args: {
+    type: TypeKey.AVAILABLE,
+  },
+}
+
 export const InProgress: Story = {
   args: {
     type: TypeKey.IN_PROGRESS,
   },
 }
 
+export const Completed: Story = {
+  args: {
+    type: TypeKey.COMPLETED,
+  },
+}
+
 export const SoldOut: Story = {
   args: {
     type: TypeKey.SOLD_OUT,
+  },
+}
+
+export const Expired: Story = {
+  args: {
+    type: TypeKey.EXPIRED,
+  },
+}
+
+export const AvailableOutline: Story = {
+  args: {
+    type: TypeKey.AVAILABLE,
+    outline: true,
+  },
+}
+
+export const InProgressOutline: Story = {
+  args: {
+    type: TypeKey.IN_PROGRESS,
+    outline: true,
+  },
+}
+
+export const CompletedOutline: Story = {
+  args: {
+    type: TypeKey.COMPLETED,
+    outline: true,
+
+  },
+}
+
+export const SoldOutOutline: Story = {
+  args: {
+    type: TypeKey.SOLD_OUT,
+    outline: true,
+  },
+}
+
+export const ExpiredOutline: Story = {
+  args: {
+    type: TypeKey.EXPIRED,
+    outline: true,
   },
 }
