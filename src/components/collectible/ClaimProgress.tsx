4fde167691ac797c2ed6c1dbe90afadc64f87c98
Spencer Miller <dogpantz@gmail.com>
2023-11-09 00:11:40 -0700

    update ClaimProgress component to match new designs

--- src/components/collectible/ClaimProgress.tsx
+++ src/components/collectible/ClaimProgress.tsx
@@ -1,27 +1,45 @@
 import { Typography } from '@mui/material'
 import LinearProgress from '@mui/material/LinearProgress'
 import { calculateProgressPercentage } from 'utils/calculate-progress-percentage'
+import { Collectible } from 'types/Collectible'
+import { getAvailableCodes } from 'utils/collectible'
 
 import styled from 'styled-components'
 
 type Props = {
   className?: string,
-  claimed: number,
-  totalAvailable: number
+  collectible: Collectible
 }
 
-const ClaimProgress = styled(({ className, claimed, totalAvailable } : Props) => (
-  <div className={className}>
-    <Typography variant="h6">
-      {`Claimed: ${claimed} / ${totalAvailable}`}
-    </Typography>
-    <LinearProgress variant="determinate" value={calculateProgressPercentage(claimed, totalAvailable)} />
-  </div>
-))`
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    max-width: 400px;
+const ClaimProgress = styled<React.FC<Props>>(({ className, collectible } : Props) => {
+  const availableCodes = getAvailableCodes(collectible)
+
+  const text = availableCodes > 99
+    ? '100+ Left'
+    : `${Math.max(availableCodes, 0)} Left`
+
+  const progress = calculateProgressPercentage(availableCodes, collectible?.total_count || 0)
+
+  return (
+    <div className={className}>
+      <Typography variant="h6">
+        {text}
+      </Typography>
+      <LinearProgress className={progress < 30 ? 'urgent' : ''} variant="determinate" value={progress} />
+    </div>
+  )
+})`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  max-width: 400px;
+
+
+  .urgent {
+    .MuiLinearProgress-bar {
+      background-color: ${({ theme }) => theme.progress.urgent.foreground};
+    }
+  }
 `
 
 export default ClaimProgress
