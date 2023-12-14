6cb1f0d7784055249c838888dc35c0b2de3db36f
Danny North <iamdnorth@gmail.com>
2023-12-14 16:02:55 -0700

    Update to use react-error-boundary

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,52 +1,44 @@
 import React from 'react'
 import { captureException, setContext } from '@sentry/nextjs'
 import { styled } from 'theme'
+import {
+  ErrorBoundary as ReactErrorBoundary, ErrorBoundaryContextType, ErrorBoundaryPropsWithComponent, useErrorBoundary, FallbackProps,
+} from 'react-error-boundary'
 
-type State = {
-  hasError: boolean
+const logError = (error: Error, info: React.ErrorInfo) => {
+  setContext('Component ErrorInfo', info)
+
+  captureException(
+    error instanceof Error ? error : new Error(
+      typeof error === 'string' ? error : 'Unknown error',
+    ),
+  )
 }
 
-type Props = React.PropsWithChildren
+type defaultFallbackComponentProps = ErrorBoundaryContextType & {
+  className?: string
+}
 
-// This hardcoded color is only here in case the theme fails to load
-// it has been blessed for this use case, but don't do this anywhere else.
-const ErrorMessage = styled.p`
+const defaultFallbackComponent = styled(({ className }: defaultFallbackComponentProps) => (
+  <div className={className}>
+    {'An error has occurred! Please try again.'}
+  </div>
+))`
+  display: flex;
+  // This hardcoded color is only here in case the theme fails to load
+  // it has been blessed for this use case, but don't do this anywhere else.
   color: ${({ theme }) => theme?.text?.error || '#FF4D49'};
 `
 
-export class ErrorBoundary extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = { hasError: false }
-  }
+export function ErrorBoundary({ FallbackComponent, children } : ErrorBoundaryPropsWithComponent) {
+  return (
+    <ReactErrorBoundary onError={logError} FallbackComponent={FallbackComponent || defaultFallbackComponent}>
+      {children}
+    </ReactErrorBoundary>
+  )
+}
 
-  static getDerivedStateFromError() {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true }
-  }
-
-  componentDidCatch(error: Error, info: React.ErrorInfo) {
-    // Example "componentStack":
-    //   in ComponentThatThrows (created by App)
-    //   in ErrorBoundary (created by App)
-    //   in div (created by App)
-    //   in App
-    setContext('Component ErrorInfo', info)
-
-    captureException(
-      error instanceof Error ? error : new Error(
-        typeof error === 'string' ? error : 'Unknown error',
-      ),
-    )
-  }
-
-  render() {
-    const { hasError } = this.state
-    const { children } = this.props
-    if (hasError) {
-      return <ErrorMessage>{'An error has occurred! Please try again.'}</ErrorMessage>
-    }
-
-    return children
-  }
+export {
+  useErrorBoundary,
+  FallbackProps,
 }
