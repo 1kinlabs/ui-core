ebb5dc27f714e5c18a2aec6f14c68ddfb18e6935
Spencer Miller <dogpantz@gmail.com>
2024-02-03 11:40:29 -0700

    get twitter login 'working' (needs testing on prod :cowboy:)

--- src/1sdk/components/SocialButtons/Twitter/index.tsx
+++ src/1sdk/components/SocialButtons/Twitter/index.tsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components'
+import TwitterLogin from 'react-twitter-auth'
+import { captureException } from '@sentry/nextjs'
+import { useAuth } from '1sdk/context/AuthContext'
+import { BACKEND_BASE_API_URL } from '1sdk/data/request'
 import Button from '../BaseButton'
 import TwitterIcon from './Icon'
 
@@ -6,15 +10,34 @@ type Props = {
   className?: string
 }
 
-const loginWithTwitter = () => {
-  console.log('login with Twitter')
-}
+const Twitter = styled(({ className }: Props) => {
+  const { loginByTwitter } = useAuth()
 
-const Twitter = styled(({ className }: Props) => (
-  <Button className={className} onClick={loginWithTwitter}>
-    <TwitterIcon />
-  </Button>
-))`
+  const loginWithTwitter = (response: Response) => {
+    response.json().then(loginByTwitter).catch((error) => {
+      console.error(error)
+      captureException(error)
+    })
+  }
+
+  return (
+    <Button
+      className={className}
+      onClick={(e: React.MouseEvent<HTMLButtonElement>) => e.preventDefault()}
+    >
+      <TwitterLogin
+        loginUrl={`${BACKEND_BASE_API_URL}/end-users/auth/twitter`}
+        /* @ts-expect-error the types on this package are wrong >_> */
+        onSuccess={loginWithTwitter}
+        onFailure={() => {}}
+        requestTokenUrl={`${BACKEND_BASE_API_URL}/end-users/auth/twitter/reverse`}
+        style={{ display: 'contents', cursor: 'pointer' }}
+      >
+        <TwitterIcon />
+      </TwitterLogin>
+    </Button>
+  )
+})`
 &&& {
   background-color: ${({ theme }) => theme.brand.twitter};
   &:hover {
