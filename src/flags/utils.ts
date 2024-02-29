c6c63327f07ec4f7b7d3a3020d9c82ca692fea4d
Danny North <iamdnorth@gmail.com>
2024-02-29 10:47:24 -0700

    Update devkin tools and create exportable UI element

--- src/flags/utils.ts
+++ src/flags/utils.ts
@@ -3,6 +3,12 @@ import { FeatureFlags } from 'types/FeatureFlags'
 
 export const FEATURE_FLAG_STORAGE_KEY = '1kin-feature-flags'
 
+export const hasFeatureFlagOverrides = () : boolean => {
+  const localFlags = getFeatureFlagOverrides()
+
+  return Object.keys(localFlags).length !== 0
+}
+
 export const getFeatureFlagOverrides = () : FeatureFlags => {
   const featureFlagOverrides = window.localStorage.getItem(FEATURE_FLAG_STORAGE_KEY)
 
@@ -18,7 +24,7 @@ const setFeatureFlagOverrides = (newFlags: FeatureFlags) => {
   window.localStorage.setItem(FEATURE_FLAG_STORAGE_KEY, JSON.stringify(newFlags))
 }
 
-export const overrideFeatureFlag = (flagToOverride: string, value: boolean) => {
+export const setFeatureFlagOverride = (flagToOverride: string, value: boolean) => {
   const localFlags = getFeatureFlagOverrides()
 
   const updatedFlags = {
