be3964ce53b5ff6abec393aefac3aece4b3687f7
Danny North <iamdnorth@gmail.com>
2024-04-29 15:55:45 -0600

    Update dialog component to allow min/max widths

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -6,17 +6,20 @@ import {
 
 import bannerImage1Kin from '../images/banners/1Kin_banner_compressed.jpg'
 
-export type Props = Omit<DialogProps, 'onClose'> & {
+export type Props = Omit<DialogProps, 'onClose' | 'maxWidth'> & {
   className?: string,
   includeBanner?: boolean,
   children?: React.ReactNode
-  onClose: () => void
+  onClose: () => void,
+  maxWidth?: number,
+  minWidth?: number
 }
 
-const DialogCard = styled(Card)`
+const DialogCard = styled(Card)<{ $minWidth?: number; $maxWidth?: number }>`
   display: flex;
   flex-direction: column;
-  max-width: 1140px;
+  min-width: ${({ $minWidth }) => ($minWidth ? `${$minWidth}px` : 'auto')};
+  max-width: ${({ $maxWidth }) => ($maxWidth ? `${$maxWidth}px` : '1140px')};
 `
 
 const DialogCardContent = styled(CardContent)`
@@ -24,10 +27,10 @@ const DialogCardContent = styled(CardContent)`
 `
 
 const Dialog = styled(({
-  className, includeBanner, open, onClose, children,
+  className, includeBanner, open, onClose, children, minWidth, maxWidth,
 } : Props) => (
   <DialogBase PaperProps={{ sx: { maxWidth: 'unset' } }} className={className} open={open} onClose={() => onClose?.()} sx={{ containerType: 'inline-size' }}>
-    <DialogCard>
+    <DialogCard $minWidth={minWidth} $maxWidth={maxWidth}>
       { includeBanner && <Banner src={bannerImage1Kin.src} alt="1Kin banner" /> }
       <DialogCardContent>
         {children}
