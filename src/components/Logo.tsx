b2a3a83d845c2b35f9f0094ea3582ca7095b2a27
Spencer Miller <dogpantz@gmail.com>
2024-07-02 15:51:58 -0600

    add TerminalFlat logo, refactor Logo component and use flat logo in nav (#107)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,18 +1,40 @@
 import { styled } from 'theme'
 import { Logo as SVGLogo } from 'svg/Logo'
+import { TerminalLogo } from 'svg/TerminalLogo'
+import { TerminalLogoFlat } from 'svg/TerminalLogoFlat'
 import { LogoSmall } from 'svg/LogoSmall'
+
+export enum Logos {
+  terminal,
+  terminalFlat,
+  brand,
+  brandSmall,
+}
 
 type Props = {
   className?: string
+  /** @deprecated use type={Logos.small} instead */
   small?: boolean
+  type?: Logos
 }
 
-export const Logo = styled(({ className, small }: Props) => {
+export const Logo = styled(({ className, small, type = Logos.terminal }: Props) => {
   if (small) {
     return <LogoSmall className={className} />
   }
 
-  return <SVGLogo className={className} />
+  switch (type) {
+    case Logos.terminal:
+      return <TerminalLogo className={className} />
+    case Logos.terminalFlat:
+      return <TerminalLogoFlat className={className} />
+    case Logos.brand:
+      return <SVGLogo className={className} />
+    case Logos.brandSmall:
+      return <LogoSmall className={className} />
+    default:
+      return <TerminalLogo className={className} />
+  }
 })``
 
 export default Logo
