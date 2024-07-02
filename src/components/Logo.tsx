7edb7eb77eed33d4008fdccec79b75dabcad0774
Spencer Miller <dogpantz@gmail.com>
2024-07-02 16:10:43 -0600

    add individual logos too

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,14 @@
 import { styled } from 'theme'
 import { Logo as SVGLogo } from 'svg/Logo'
+import { ArenaLogo } from 'svg/ArenaLogo'
+import { ArenaLogoFlat } from 'svg/ArenaLogoFlat'
 import { TerminalLogo } from 'svg/TerminalLogo'
 import { TerminalLogoFlat } from 'svg/TerminalLogoFlat'
 import { LogoSmall } from 'svg/LogoSmall'
 
 export enum Logos {
   arena,
+  arenaFlat,
   brand,
   brandSmall,
   terminal,
@@ -25,14 +28,18 @@ export const Logo = styled(({ className, small, type = Logos.terminal }: Props) 
   }
 
   switch (type) {
-    case Logos.terminal:
-      return <TerminalLogo className={className} />
-    case Logos.terminalFlat:
-      return <TerminalLogoFlat className={className} />
+    case Logos.arena:
+      return <ArenaLogo className={className} />
+    case Logos.arenaFlat:
+      return <ArenaLogoFlat className={className} />
     case Logos.brand:
       return <SVGLogo className={className} />
     case Logos.brandSmall:
       return <LogoSmall className={className} />
+    case Logos.terminal:
+      return <TerminalLogo className={className} />
+    case Logos.terminalFlat:
+      return <TerminalLogoFlat className={className} />
     default:
       return <TerminalLogo className={className} />
   }
